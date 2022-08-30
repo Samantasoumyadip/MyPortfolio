158c50d0f291310072ed43c4a687cdf1a897b3a8
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2022-08-31 01:17:52 +0530

    about part changed

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,8 +11,8 @@ class About extends Component {
                     <span className={classes.head}>ABOUT ME</span>
                     <h2 className={classes.heading}>Who Am I?</h2>
                     <div className={classes.About}>
-                        <p> My name is <b>SACHIN MITTAL</b> and I am currently working as Software Developer Engineer at <a target="_blank" href="https://www.springworks.in/"><b>Springworks</b></a>. I completed my degree in Bachelor of Technology in Computer Science and Engineering from Sant Longowal Institute of Engineering and Technology. I am much interested in developing new things which excite me a lot. :)  </p>
-                        <p className={classes.br}>I love exploring new technologies and being a practitioner, I like to stay on top of latest trends. I try to leave every line of code I write more readable, accessible, and modular.
+                        <p> My name is <b>SOUMYADIP SAMANTA</b> and I am currently a Student in <a target="_blank" href="https://www.springworks.in/"><b>BIT MESRA</b></a>. I am in my final year of degree in Integrated Master of Science in Mathematics and Computing. I am much interested in developing new things which excite me a lot. :)  </p>
+                        <p className={classes.br}>I love exploring new technologies and being a student of computing line, I like to stay on top of latest trends. I try to leave every line of code I write more readable, accessible, and modular.
                         </p>
                     </div>
                 </ScrollAnimation>
