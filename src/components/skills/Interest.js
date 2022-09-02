17cc6d05dba9a46b3326c9b99cf16910559d1a9e
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2022-09-02 15:15:10 +0530

    interest done

--- src/components/skills/Interest.js
+++ src/components/skills/Interest.js
@@ -11,12 +11,12 @@ class Interest extends Component {
           <span className={classes.head}>WHAT I LOVE</span>
           <h2 className={classes.heading}>HERE ARE SOME OF MY SKILLS</h2>
           <div className={classes.Interest}>
-            {/* <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
+            { <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.web}>
-                <h3>APP Develpoment</h3>
-                <p>I also have knowledge of flutter development and have experience in building android and IOS applications. I also have live app published on Google Play Store. </p>
+                <h3>Problem Solving</h3>
+                <p>I am loving my journey in problem solving ability in DSA. I do practice solving problems of Data Structure and Algorithm in Leetcode. I have solved around 200 questons till now in Leetcode and GFG. </p>
               </div>
-            </ScrollAnimation> */}
+            </ScrollAnimation> }
             <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.app}>
                 <h3>Web Develpoment</h3>
@@ -25,8 +25,8 @@ class Interest extends Component {
             </ScrollAnimation>
             <ScrollAnimation offset={0} animateIn="fadeInLeft" duration={2.4} animateOnce={true} initiallyVisible={true}>
               <div className={classes.other}>
-                <h3>Other's Interest</h3>
-                <p>I am interested in problem solving skills<br />C++, Github are my other fields of interest. Currently, I am investing my time in Data Structures and Algorithm.</p>
+                <h3>Other Interests</h3>
+                <p>I am quite interested in playing and watching football.And from my childhood cosmos has been my area of exploration. I do explore a lot of information about cosmos.</p>
               </div>
             </ScrollAnimation>
           </div>
